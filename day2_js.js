294fa44f0405da497a08c48eadb49d1329d49982
sindhu070 <gorthi.17.it@anits.edu.in>
2020-06-30 17:33:44 +0530

    day2 js tasks

---
+++ day2_js.js
@@ -0,0 +1,5 @@
+date=document.prompt("Enter a date of your choice");
+var d=new Date();
+document.write(setDate(date));
+document.write("Today's date is :");
+document.getElementById("h1").innerHTML=document.write(d.Date());
